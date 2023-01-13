34bcc427310a51ddbf8a3d76fc5ee55d43752fae
Rakesh <rakesh33d@gmail.com>
2023-01-13 20:40:35 +0530

    created product class inside main

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,20 +1,21 @@
 package com.driver;
 
 public class Main {
+
+    class Product{
+        public int product(int x,int y){
+            return x*y;
+        }
+        public int product(int x,int y,int z){
+            return x*y*z;
+        }
+        public double product(double x,double y){
+            return x*y;
+        }
+    }
 Product p = new Product();
 int a= p.product(2,3);
 int b=p.product(2,3,4);
 double c=p.product(3.0,4.0);
 
-}
-class Product{
-   public int product(int x,int y){
-        return x*y;
-    }
-    public int product(int x,int y,int z){
-       return x*y*z;
-    }
-    public double product(double x,double y){
-       return x*y;
-    }
 }
