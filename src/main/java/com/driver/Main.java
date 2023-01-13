42d15ad66f2f60138ad763f4965a5669034dca77
Rakesh <rakesh33d@gmail.com>
2023-01-13 19:35:41 +0530

    created product class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
 public class Main {
+Product p = new Product();
+int a= p.product(2,3);
+int b=p.product(2,3,4);
+double c=p.product(3.0,4.0);
 
+}
+class Product{
+   public int product(int x,int y){
+        return x*y;
+    }
+    public int product(int x,int y,int z){
+       return x*y*z;
+    }
+    public double product(double x,double y){
+       return x*y;
+    }
 }
